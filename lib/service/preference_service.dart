5cf92e6180cb35d6bcb71e4dd637fa039c7a7f4d
Ziyoviddin <ziyo07070@gmail.com>
2023-07-17 17:14:36 +0500

    add style folder files and preference_service.dart

---
+++ lib/service/preference_service.dart
@@ -0,0 +1,15 @@
+import 'package:get_storage/get_storage.dart';
+
+class PreferenceService {
+  final _box = GetStorage();
+
+
+
+ /* void setToken(String token) {
+    _box.write(IConstanta.USER_TOKEN, token);
+  }
+
+  getToken() {
+    return _box.read(IConstanta.USER_TOKEN) ?? "";
+  }*/
+}
